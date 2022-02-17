0f233be23e904a3861a52319500d95e94b19733a
JSriwongsa <jsriwongsa@hawk.iit.edu>
2022-02-16 19:43:30 -0600

    added a second file of python conditional expressions

---
+++ py2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+#Conditionals and Booleans
+
+if True:
+#if False:
+    language = 'JavaScript' 
+if language == 'Python':
+    print('Conditional was true')
+    print('Language is Python')
+elif language == 'Java':
+    print('Language is Java')
+elif language == 'JavaScript':
+   print('Language is JavaScript')
+else:     
+   print('No match')
+
+#and = making sure that 2 conditions are true
+#or = Don't care that 2 conditions are true, if 1 condition is true which means true
+#not = Change conditions 
+#user = 'Admin'
+#logged_in = False
+
+#if user == 'Admin' or logged_in:
+    #print('Admin Page')
+#else:
+    #print('Bad Creds')
+#if not logged_in:
+    #print('Please Log In')
+#else:
+    #print('Welcome')
+
+#a = [1, 2, 3]
+#b = a
+#b = [1, 2, 3]
+
+#print(id(a))
+#print(id(b))
+#print(a is b)
+##---------------------------------------------------------------------------##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
