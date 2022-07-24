4b674bef37e71e3f713890fd6832d7d44734b750
jsriwongsa <jsriwongsa@hawk.iit.edu>
2022-07-23 23:24:16 -0500

    Added BMI

---
+++ BMI.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+x = input("Please input your weight in kilogram: ") #error
+y = input("Please input your height in centimeter: ") #error
+
+def calculate(x, y):
+    x = float(x)
+    y =float(y)
+    return((x/(y/100)) / (y/100))
+
+result = calculate(x, y)
+
+print("Your height entered {} in centimeter (cm)".format(x))
+print("Your weight entered {} in kilogram (kg)".format(y))
+print("Your BMI is {}".format(result))
+
+
